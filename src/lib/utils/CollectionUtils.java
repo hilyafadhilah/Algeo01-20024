4fadf942fcea5faca721b442f03de1fefd8aad68
Hilya Fadhilah Imania <hilyafadhilah@gmail.com>
2021-09-27 20:19:20 +0700

    add utils documentation

--- src/lib/utils/CollectionUtils.java
+++ src/lib/utils/CollectionUtils.java
@@ -3,7 +3,24 @@ package lib.utils;
 import java.util.Collection;
 import java.util.function.Predicate;
 
+/**
+ * Utilities for <code>Collection</code>.
+ * 
+ * @author Hilya Fadhilah Imania<hilyafadhilah@gmail.com>
+ * @version 0.1.1
+ * @see java.util.Collection
+ * @since 2021-09-26
+ */
 public final class CollectionUtils {
+  /**
+   * Find using a lambda <code>filter</code> in <code>col</code>.
+   * 
+   * @param <T>    Type of collection elements
+   * @param col    The collection
+   * @param filter The lambda
+   * @return The element which returns <code>true</code> when passed to
+   *         <code>filter</code>, <code>null</code> if not found
+   */
   public static <T> T find(Collection<T> col, Predicate<T> filter) {
     return col.stream().filter(filter).findFirst().orElse(null);
   }
--- src/lib/utils/IOUtils.java
+++ src/lib/utils/IOUtils.java
@@ -5,30 +5,96 @@ import java.util.Scanner;
 
 import lib.matrix.Matrix;
 
+/**
+ * Utilities for I/O, specific to this application.
+ * 
+ * @author Hilya Fadhilah Imania<hilyafadhilah@gmail.com>
+ * @version 0.1.4
+ * @see java.util.Collection
+ * @since 2021-09-26
+ */
 public class IOUtils {
+  /**
+   * <code>Scanner</code> of <code>stdin</code>.
+   * 
+   * @see java.util.Scanner
+   */
   protected static Scanner stdinScanner;
 
+  /**
+   * Initializes I/O. Opens <code>stdin</code> scanner.
+   * 
+   * @see #stdinScanner
+   */
   public static void init() {
     stdinScanner = new Scanner(System.in);
   }
 
+  /**
+   * Cleans up I/O. Closes <code>stdin</code> scanner.
+   * 
+   * @see #init()
+   */
   public static void close() {
     stdinScanner.close();
   }
 
+  /**
+   * Getter for <code>stdinScanner</code>.
+   * 
+   * @return <code>stdin</code> scanner instance
+   * @see #stdinScanner
+   */
   public static Scanner getStdinScanner() {
     return stdinScanner;
   }
 
+  /**
+   * Inputs from stdin with a message.
+   * 
+   * @param query Message to be shown. No space at the end required.
+   * @return The user input
+   */
   public static String prompt(String query) {
     System.out.print(query + " ");
     return stdinScanner.nextLine();
   }
 
+  /**
+   * Inputs from stdin without a message
+   * 
+   * @return The user input
+   */
   public static String prompt() {
     return stdinScanner.nextLine();
   }
 
+  /**
+   * <p>
+   * Asks the user to input a <code>Matrix</code>.
+   * </p>
+   * 
+   * <p>
+   * First, this function assumes that the user inputs two integers separated by
+   * whitespace in a line, which denotes the row and column length of the matrix.
+   * If it successfully does that, then it tries to input matrix from stdin using
+   * the specified size.
+   * </p>
+   * 
+   * <p>
+   * If it fails to read two integers, it assumes that the whole line is a
+   * filename. Then it tries to read the file.
+   * </p>
+   * 
+   * <p>
+   * If it fails to read the file, an exception will be thrown automatically.
+   * </p>
+   * 
+   * @return
+   * @throws Exception If input is invalid
+   * @see lib.matrix.Matrix#read(Scanner, int, int)
+   * @see lib.matrix.Matrix#read(Scanner)
+   */
   public static Matrix inputMatrix() throws Exception {
     File file = null;
     Matrix mat = null;
@@ -53,6 +119,11 @@ public class IOUtils {
     return mat;
   }
 
+  /**
+   * Display a header with a specified title.
+   * 
+   * @param title The title
+   */
   public static void printHeader(String title) {
     int n = (60 - title.length() - 4) / 2;
     String header = "";
@@ -67,6 +138,13 @@ public class IOUtils {
     System.out.print(header);
   }
 
+  /**
+   * Repeats a string, <code>n</code> number of times.
+   * 
+   * @param str The string to be repeated
+   * @param n   The number of repetition
+   * @return The repeated string
+   */
   public static String repeat(String str, int n) {
     String result = "";
     for (int i = 0; i < n; i++) {
