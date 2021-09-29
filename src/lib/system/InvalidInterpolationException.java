2ab5f43a70dedd27205de2b71bad0dc102d54045
Hilya Fadhilah Imania <hilyafadhilah@gmail.com>
2021-09-29 19:58:54 +0700

    add interpolation input

---
+++ src/lib/system/InvalidInterpolationException.java
@@ -0,0 +1,12 @@
+package lib.system;
+
+/**
+ * Exception for invalid interpolation, usually upon input
+ * 
+ * @author Hilya Fadhilah Imania
+ * @version 0.1.0
+ * @since 2021-09-28
+ */
+public class InvalidInterpolationException extends Exception {
+  /* stub */
+}
