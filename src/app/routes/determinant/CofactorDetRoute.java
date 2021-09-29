833c70333712776ffef7b4cb81b433b8db67b1b9
Hilya Fadhilah Imania <hilyafadhilah@gmail.com>
2021-09-29 20:50:50 +0700

    cofactor determinant route

--- src/app/routes/determinant/CofactorDetRoute.java
+++ src/app/routes/determinant/CofactorDetRoute.java
@@ -1,9 +1,18 @@
 package app.routes.determinant;
 
+import lib.matrix.Determinant;
+import lib.matrix.Matrix;
 import lib.router.Route;
+import lib.utils.IOUtils;
 
 public class CofactorDetRoute extends Route {
   public CofactorDetRoute(String key) {
     super(key, "Metode Kofaktor");
+  }
+
+  public void run() throws Exception {
+    Matrix m = IOUtils.inputMatrix(true);
+    double det = Determinant.cofactorMethod(m);
+    System.out.println("\nDeterminan matriks = " + det);
   }
 }
