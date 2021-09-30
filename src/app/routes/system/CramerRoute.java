2ad9fed3dab38a1a05ec0fd288df64fd499916c2
jundanha <jundanha@gmail.com>
2021-09-30 11:49:18 +0700

    crammer

--- src/app/routes/system/CramerRoute.java
+++ src/app/routes/system/CramerRoute.java
@@ -2,8 +2,24 @@ package app.routes.system;
 
 import lib.router.Route;
 
+import java.util.ArrayList;
+
+import lib.matrix.Matrix;
+import lib.utils.InputUtils;
+import lib.system.LinearSystem;
+
 public class CramerRoute extends Route {
   public CramerRoute(String key) {
     super(key, "Kaidah Cramer");
+  }
+
+  public void run() throws Exception {
+    Matrix m = InputUtils.inputMatrix();
+    ArrayList<Double> result = LinearSystem.cramerMethod(m);
+    System.out.println("Hasil SPL dengan Metode Cramer : ");
+    System.out.println();
+    for (int i = 0; i < result.size(); i++) {
+      System.out.println("x_" + i + " = " + result.get(i));
+    }
   }
 }
