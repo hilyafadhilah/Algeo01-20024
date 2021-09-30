1491e96f54f66f8b9b86fce9dd0a259e02294ad8
Hilya Fadhilah Imania <hilyafadhilah@gmail.com>
2021-09-30 17:29:33 +0700

    refactor, add solution class

--- src/lib/system/LinearSystem.java
+++ src/lib/system/LinearSystem.java
@@ -1,6 +1,7 @@
 package lib.system;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 import lib.matrix.Determinant;
 import lib.matrix.Matrix;
@@ -11,6 +12,7 @@ import lib.system.CannotSolveException.Method;
 /**
  * Linear equation system solver
  * 
+ * @author Roby Purnomo
  * @author Jundan Haris
  * @version 0.1.0
  * @see lib.matrix.Matrix
@@ -24,6 +26,100 @@ public class LinearSystem {
     UNIQUE, INFINITE, NONE
   }
 
+  private static final char alfabet[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+      'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+  protected Matrix m;
+
+  public static Solution[] gaussUnique(Matrix mEchelon) {
+    int nVars = mEchelon.getNCols() - 1;
+    Solution[] solutions = new Solution[nVars];
+
+    for (int i = nVars - 1; i >= 0; i--) {
+      solutions[i] = new Solution(mEchelon.get(i, nVars));
+      for (int j = i + 1; j < mEchelon.getNRows(); j++) {
+        solutions[i].constant -= mEchelon.get(i, j) * solutions[j].constant;
+      }
+    }
+
+    return solutions;
+  }
+
+  public static Solution[] gaussInfinite(Matrix mEchelon) {
+
+    int nCols = mEchelon.getNCols();
+    Solution[] solutions = new Solution[nCols - 1];
+    Vector<int[]> pivots = mEchelon.getPivots();
+    int[] pivotMap = new int[nCols - 1];
+
+    // Check if each columns contains a pivot
+    // Keep in mind, column with no pivot is a parameter
+    for (int j = 0; j < nCols - 1; j++) {
+      pivotMap[j] = -1;
+      for (int[] pivot : pivots) {
+        if (pivot[1] == j) {
+          pivotMap[j] = pivot[0];
+          break;
+        }
+      }
+    }
+
+    // Loop through solutions, number of solutions = columns - 1
+    int idxAlfabet = 0;
+    for (int j = 0; j < nCols - 1; j++) {
+      if (pivotMap[j] == -1) {
+        // This variable is a parameter
+        String param = Character.toString(alfabet[idxAlfabet % alfabet.length]);
+        int paramSeq = idxAlfabet / alfabet.length;
+
+        if (paramSeq > 0) {
+          param += "_" + paramSeq;
+        }
+
+        solutions[j] = new Solution(param);
+        idxAlfabet++;
+      } else {
+        // This variable is not a parameter
+        Vector<Double> row = mEchelon.getRowCopy(pivotMap[j]);
+
+        // Do backwards substitution
+        for (int i = j + 1; i < nCols - 1; i++) {
+          if (pivotMap[i] == -1) {
+            // This variable is not a parameter, subtitute the real value
+            for (int k = 0; k < nCols; k++) {
+              double tempVal = row.get(k) - mEchelon.get(j, i) * mEchelon.get(i, k);
+              row.set(k, tempVal);
+            }
+          }
+        }
+
+        // Rightmost value is now the final constant
+        solutions[j] = new Solution(row.get(nCols - 1));
+
+        // Insert params
+        int paramIdx = 0;
+        for (int i = j + 1; i < nCols - 1; i++) {
+          if (pivotMap[i] == -1) {
+            // This variable is a parameter
+            double coeff = -row.get(i);
+
+            String param = Character.toString(alfabet[paramIdx % alfabet.length]);
+            int paramSeq = paramIdx / alfabet.length;
+
+            if (paramSeq > 0) {
+              param += "_" + paramSeq;
+            }
+
+            solutions[j].add(param, coeff);
+            paramIdx++;
+          }
+        }
+      }
+    }
+
+    return solutions;
+  }
+
   /**
    * Solve linear system using Gauss Jordan Method only if the solution is unique.
    * 
@@ -31,18 +127,67 @@ public class LinearSystem {
    * @return List of solutions
    * @throws Exception If solution is not unique
    */
-  public static double[] gaussJordanUnique(Matrix m) throws Exception {
-    Matrix mRed = m.toReducedEchelon();
+  public static Solution[] gaussJordanUnique(Matrix mRed) {
+    int nVars = mRed.getNCols() - 1;
+    Solution[] solutions = new Solution[nVars];
 
-    if (checkSolutionType(mRed) != SolutionType.UNIQUE) {
-      throw new Exception();
+    for (int i = 0; i < nVars; i++) {
+      solutions[i] = new Solution(mRed.get(i, nVars));
     }
 
-    int nCols = mRed.getNCols();
-    double[] solutions = new double[nCols - 1];
+    return solutions;
+  }
 
-    for (int i = 0; i < nCols - 1; i++) {
-      solutions[i] = mRed.get(i, nCols - 1);
+  public static Solution[] gaussJordanInfinite(Matrix mRed) {
+    int nCols = mRed.getNCols();
+    Solution[] solutions = new Solution[nCols - 1];
+    Vector<int[]> pivots = mRed.getPivots();
+
+    // Check if each columns contains a pivot
+    // Keep in mind, column with no pivot is a parameter
+    boolean[] columnHasPivot = new boolean[nCols - 1];
+    for (int j = 0; j < nCols - 1; j++) {
+      columnHasPivot[j] = false;
+      for (int[] pivot : pivots) {
+        if (pivot[1] == j) {
+          columnHasPivot[j] = true;
+          break;
+        }
+      }
+    }
+
+    // Loop through solutions, number of solutions = columns - 1
+    int idxAlfabet = 0;
+    for (int j = 0; j < mRed.getNCols() - 1; j++) {
+      if (!columnHasPivot[j]) {
+        // This variable is a parameter
+        String param = Character.toString(alfabet[idxAlfabet % alfabet.length]);
+        int paramSeq = idxAlfabet / alfabet.length;
+
+        if (paramSeq > 0) {
+          param += "_" + paramSeq;
+        }
+
+        solutions[j] = new Solution(param);
+        idxAlfabet++;
+      } else {
+        // This variable is not a parameter
+        solutions[j] = new Solution(mRed.get(j, nCols - 1));
+        int paramIdx = 0;
+        for (int k = j + 1; k < nCols - 1; k++) {
+          double coeff = -mRed.get(j, k);
+
+          String param = Character.toString(alfabet[paramIdx % alfabet.length]);
+          int paramSeq = paramIdx / alfabet.length;
+
+          if (paramSeq > 0) {
+            param += "_" + paramSeq;
+          }
+
+          solutions[j].add(param, coeff);
+          paramIdx++;
+        }
+      }
     }
 
     return solutions;
@@ -84,6 +229,21 @@ public class LinearSystem {
     return solution;
   }
 
+  public static Matrix InverseMethod(Matrix m) throws Exception {
+    Matrix mA = new Matrix(m.getNRows(), m.getNCols() - 1);
+    Matrix mB = new Matrix(m.getNRows(), 1);
+
+    for (int a = 0; a < m.getNRows(); a++) {
+      mB.set(a, 0, m.get(a, m.getNCols() - 1));
+      for (int b = 0; b < mA.getNCols(); b++) {
+        mA.set(a, b, m.get(a, b));
+      }
+    }
+    Matrix mAInvers = Inverse.CofactorMethod(mA);
+    Matrix mResult = mAInvers.multiplyMatrix(mB);
+    return mResult;
+  }
+
   /**
    * Returns a complete augmented matrix version of a linear system. The number of
    * rows should be at least equal to the number of columns minus 1.
@@ -111,45 +271,31 @@ public class LinearSystem {
    * @return SolutionType type
    */
   public static SolutionType checkSolutionType(Matrix mEchelon) {
-    boolean isAllZero = true;
-    int i;
-    for (i = mEchelon.getNRows() - 1; i >= 0 && isAllZero; i--) {
-      isAllZero = true;
-      for (int j = 0; j <= mEchelon.getNCols() - 1 && isAllZero; j++) {
-        if (mEchelon.get(i, j) != 0) {
-          isAllZero = false;
+    int nRows = mEchelon.getNRows();
+    int nCols = mEchelon.getNCols();
+    int nVars = nCols - 1;
+
+    if (nRows < nVars) {
+      return SolutionType.INFINITE;
+    }
+
+    double constant = mEchelon.get(nVars - 1, nCols - 1);
+    double coeff = mEchelon.get(nVars - 1, nCols - 2);
+
+    if (coeff == 0.0) {
+      if (constant != 0.0) {
+        return SolutionType.NONE;
+      } else {
+        return SolutionType.INFINITE;
+      }
+    } else if (nRows > nVars) {
+      for (int i = nVars; i < nRows; i++) {
+        if (mEchelon.get(i, nCols - 1) != 0.0) {
+          return SolutionType.NONE;
         }
       }
     }
-    i++;
-    isAllZero = true;
-    for (int j = 0; j < mEchelon.getNCols() - 1 && isAllZero; j++) {
-      if (mEchelon.get(i, j) != 0) {
-        isAllZero = false;
-      }
-    }
 
-    if (isAllZero && mEchelon.get(i, mEchelon.getNCols() - 1) != 0) {
-      return SolutionType.NONE;
-    } else if (i + 1 == mEchelon.getNCols() - 1) {
-      return SolutionType.UNIQUE;
-    } else {
-      return SolutionType.INFINITE;
-    }
-  }
-
-  public static Matrix InverseMethod (Matrix m) throws Exception {
-    Matrix mA = new Matrix(m.getNRows(), m.getNCols() - 1);
-    Matrix mB = new Matrix(m.getNRows(), 1);
-
-    for (int a = 0; a < m.getNRows(); a++) {
-      mB.set(a, 0, m.get(a, m.getNCols() - 1));
-      for (int b = 0; b < mA.getNCols(); b++) {
-        mA.set(a, b, m.get(a, b));
-      }
-    }
-    Matrix mAInvers = Inverse.CofactorMethod(mA);
-    Matrix mResult = mAInvers.multiplyMatrix(mB);
-    return mResult;
+    return SolutionType.UNIQUE;
   }
 }
---
+++ src/lib/system/Solution.java
@@ -0,0 +1,68 @@
+package lib.system;
+
+import java.util.HashMap;
+
+public class Solution {
+  public double constant = 0.0;
+  public HashMap<String, Double> parameters = new HashMap<>();
+
+  public Solution(double constant) {
+    this.constant = constant;
+  }
+
+  public Solution(String param) {
+    this.parameters.put(param, 1.0);
+  }
+
+  public Solution(String param, double coeff) {
+    this.parameters.put(param, coeff);
+  }
+
+  public void add(String param, double coeff) {
+    this.parameters.put(param, coeff);
+  }
+
+  public void get(String param) {
+    this.parameters.get(param);
+  }
+
+  public boolean hasParams() {
+    return !this.parameters.isEmpty();
+  }
+
+  public String toString() {
+    String str = "";
+
+    if (!this.hasParams()) {
+      if (this.constant == 0.0) {
+        str += 0.0; // Avoid -0.0
+      } else {
+        str += this.constant;
+      }
+    } else {
+      if (this.constant != 0.0) {
+        str += this.constant;
+      }
+
+      for (String param : this.parameters.keySet()) {
+        double coeff = this.parameters.get(param);
+
+        if (coeff != 0.0) {
+          if (coeff > 0 && !str.isEmpty()) {
+            str += " + ";
+          } else {
+            str += str.isEmpty() ? "-" : " - ";
+          }
+
+          if (Math.abs(coeff) != 1.0) {
+            str += coeff + " * ";
+          }
+
+          str += param;
+        }
+      }
+    }
+
+    return str;
+  }
+}
